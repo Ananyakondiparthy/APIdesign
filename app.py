e75441b3aac32229f5d3b1208daa9563e5a955e2
Ananya Kondiparthy <ananyakondiparthy@ananyas-MacBook-Pro-2.local>
2025-03-02 00:39:21 -0800

    entered data

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from crypt import methods
 
+from flask import Flask, jsonify, request
+#jsonify package converts python objects into JSON
+from http import HTTPStatus
+
+# creating an instance of FLASK Class
 app = Flask(__name__)
 
-@app.route("/")
+#Define the recepies list
 
-def hello():
-    return "Hello!"
+recepies = [
+    {
+
+    "id" : 1,
+    'name': 'Egg Salad',
+    'description': 'This is egg salad recipe'
+},
+    {
+        "id": 2,
+        'name': 'Tomato Salad',
+        'description': 'This is a Tomato Salad recipe'
+    }
+]
+
+#route decorator
+@app.route('/recepies', methods = ['GET'])
+
+def get_recipies():
+    return jsonify({'data': recepies})
 if __name__ == "__main__":
     app.run()
 
